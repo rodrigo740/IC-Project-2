6335455e16516b34aced371e3a390c6ceaeb47a3
luciasousa <luciamsousa00@ua.pt>
2021-11-27 12:30:13 +0000

    method decode not working, Golomb class

--- Golomb.h
+++ Golomb.h
@@ -30,8 +30,26 @@ class Golomb {
         return code;
     }
 
-    void decode(){
+    int decode(vector<int> code){
+        int n = 0;
+        int q = 0;
+        int r = 0;
+        int nbits = log2(m);
+        for(int i : code){
+            if(i!=1){
+                q++;
+                code.pop_back();
+            }
+            else{
+                code.pop_back();
+                break;
+            }
+        }
 
+        r = binaryToDecimal(code);
+        cout << "r -> " << r << endl;
+        n=q*m+r;
+        return n;
     }
 
     vector<int> decToBinary(int n, int m){
@@ -55,6 +73,29 @@ class Golomb {
         }
         binaryNum = bin;
         return binaryNum;
+    }
+
+    int binaryToDecimal(vector<int> n){
+        string s;
+        for(int i : n){
+            cout << "wtfffffffffff " << n[i] << endl;
+            string s1 = to_string(n[i]);
+            s = s + s1;
+        }
+        int num = stoi(s);
+        int dec_value = 0;
+        // Initializing base value to 1, i.e 2^0
+        int base = 1;
+        int temp = num;
+        while (temp) {
+            int last_digit = temp % 10;
+            temp = temp / 10;
+    
+            dec_value += last_digit * base;
+    
+            base = base * 2;
+        }
+        return dec_value;
     }
 
 };
--- ex2a.cpp
+++ ex2a.cpp
@@ -28,5 +28,20 @@ int main(){
     for(int i : x4){
         cout << "" << i << endl;
     }
+
+    Golomb g3(4);
+    int y1 = g3.decode(x1);
+    cout << "value: " << y1 << endl;
+
+    int y2 = g3.decode(x2);
+    cout << "value: " << y2 << endl;
+
+    Golomb g4(5);
+    int y3 = g4.decode(x3);
+    cout << "value: " << y3 << endl;
+
+    int y4 = g4.decode(x4);
+    cout << "value: " << y4 << endl;
+
     return 0;
 }
